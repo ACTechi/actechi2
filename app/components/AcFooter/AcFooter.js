0c785e074d8b99c00b445a5eca3f76eba01cc76a
laladiaz <mariaabelardadiaz@gmail.com>
2023-08-25 19:24:46 -0500

    starting the footer with material design

--- app/components/AcFooter/AcFooter.js
+++ app/components/AcFooter/AcFooter.js
@@ -1,47 +1,125 @@
-/* import React from 'react'
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
-import styles from './AcFooter.module.css'
+import React from 'react';
+import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-kit';
 
 export default function AcFooter() {
     return (
-        <footer className={styles.footer}>
-            <Container>
-                <Row>
-                    <Col md={6} className={styles.map}>
-                        <div className={styles.logoDiv}>
-                            <a href='#home'><img src='logofooter.png' alt='AC Techi logo'></img></a>
-                            <p>Your Air Conditioning and Heating Company in Orlando, FL</p>
-                        </div>
-                        <ul>
-                            <li><a href='#home'>Home</a></li>
-                            <li><a href='#services'>Services</a></li>
-                            <li><a href='#financing'>Financing</a></li>
-                            <li><a href='#faq'>FAQs</a></li>
-                        </ul>
-                    </Col>
-                    <Col sm={6} md={3} className={styles.contactUsDiv}>
-                        <p className={styles.contactUs}>Contact Us</p>
-                        <ul>
-                            <a href='tel:+14072420908' className={styles.liPhone}><img src='phone.png' alt='phone image' className={styles.img}></img>(407) 242-0908</a>
-                            <a href='tel:+13219725978' className={styles.liPhone}><img src='phone.png' alt='phone image' className={styles.img}></img>(321) 972-5978</a>
-                            <a href='mailto:info@actechi.com' className={styles.liEmail}><img src='email.png' alt='email image' className={styles.img}></img>info@actechi.com</a>
-                            <a href="//maps.apple.com/?address=2721%20Forsyth%20Rd,%20Orlando,%20FL%20%2032792,%20United%20States&ll=28.583366,-81.293533&q=AC%20Techi" className={styles.liDirection}><img src='direction.png' alt='pin location image' className={styles.img}></img>2721 Forsyth Rd #351 Winter Park, Florida 32792</a>
-                        </ul>
-                    </Col>
-                    <Col sm={6} md={3}>Bussines Hours</Col>
-                    <div className={styles.socialDiv}>
-                <p className={styles.socialP}>Visit us in our Social Media</p>
-                    <a href='https://goo.gl/maps/c1d3KRTTGKzyhPXj9?coh=178573&entry=tt' className={styles.social}><img src='googleMaps.png' alt='google maps logo' className={styles.imgMaps}></img>Google Maps</a>
-                    <a href="https://api.whatsapp.com/send/?phone=14072420908&text=I'm%20interested%20in%20more%20information%20about%20your%20services,%20please&type=phone_number&app_absent=0" className={styles.social}><img src='whatsapp.png' alt='whatsapplogo' className={styles.img}></img>Whatsapp</a>
-                    <a href='https://www.facebook.com/ac.techi.7' className={styles.social}><img src='facebook.png' alt='facebook logo' className={styles.img}></img>Facebook</a>
-                    <a href='https://www.instagram.com/actechifl/' className={styles.social}><img src='Instagram_Glyph_Gradient.png' alt='instagram logo' className={styles.img}></img>Instagram</a>
+        <MDBFooter bgColor='none' className='text-center text-lg-start text-muted' style={{ backgroundColor:'#77777A' }}>
+            <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
+                <div className='me-5 d-none d-lg-block'>
+                    <span className='text-light'>Get connected with us on social networks:</span>
+                </div>
+
+                <div>
+                    <a href='' className='me-4 text-reset'>
+                        <MDBIcon fab icon="facebook-f" />
+                    </a>
+                    <a href='' className='me-4 text-reset'>
+                        <MDBIcon fab icon="twitter" />
+                    </a>
+                    <a href='' className='me-4 text-reset'>
+                        <MDBIcon fab icon="google" />
+                    </a>
+                    <a href='' className='me-4 text-reset'>
+                        <MDBIcon fab icon="instagram" />
+                    </a>
+                    <a href='' className='me-4 text-reset'>
+                        <MDBIcon fab icon="linkedin" />
+                    </a>
+                    <a href='' className='me-4 text-reset'>
+                        <MDBIcon fab icon="github" />
+                    </a>
+                </div>
+            </section>
+
+            <section className='text-light'>
+                <MDBContainer className='text-center text-md-start mt-5'>
+                    <MDBRow className='mt-3'>
+                        <MDBCol md="3" lg="4" xl="3" className='mx-auto mb-4'>
+                            <h6 className='text-uppercase fw-bold mb-4'>
+                                <MDBIcon icon="gem" className="me-3" />
+                                Company name
+                            </h6>
+                            <p>
+                                Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
+                                consectetur adipisicing elit.
+                            </p>
+                        </MDBCol>
+
+                        <MDBCol md="2" lg="2" xl="2" className='mx-auto mb-4'>
+                            <h6 className='text-uppercase fw-bold mb-4'>Products</h6>
+                            <p>
+                                <a href='#!' className='text-reset'>
+                                    Angular
+                                </a>
+                            </p>
+                            <p>
+                                <a href='#!' className='text-reset'>
+                                    React
+                                </a>
+                            </p>
+                            <p>
+                                <a href='#!' className='text-reset'>
+                                    Vue
+                                </a>
+                            </p>
+                            <p>
+                                <a href='#!' className='text-reset'>
+                                    Laravel
+                                </a>
+                            </p>
+                        </MDBCol>
+
+                        <MDBCol md="3" lg="2" xl="2" className='mx-auto mb-4'>
+                            <h6 className='text-uppercase fw-bold mb-4'>Useful links</h6>
+                            <p>
+                                <a href='#!' className='text-reset'>
+                                    Pricing
+                                </a>
+                            </p>
+                            <p>
+                                <a href='#!' className='text-reset'>
+                                    Settings
+                                </a>
+                            </p>
+                            <p>
+                                <a href='#!' className='text-reset'>
+                                    Orders
+                                </a>
+                            </p>
+                            <p>
+                                <a href='#!' className='text-reset'>
+                                    Help
+                                </a>
+                            </p>
+                        </MDBCol>
+
+                        <MDBCol md="4" lg="3" xl="3" className='mx-auto mb-md-0 mb-4'>
+                            <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
+                            <p>
+                                <MDBIcon icon="home" className="me-2" />
+                                New York, NY 10012, US
+                            </p>
+                            <p>
+                                <MDBIcon icon="envelope" className="me-3" />
+                                info@example.com
+                            </p>
+                            <p>
+                                <MDBIcon icon="phone" className="me-3" /> + 01 234 567 88
+                            </p>
+                            <p>
+                                <MDBIcon icon="print" className="me-3" /> + 01 234 567 89
+                            </p>
+                        </MDBCol>
+                    </MDBRow>
+                </MDBContainer>
+            </section>
+
+            <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
+                © 2021 Copyright:
+                <a className='text-reset fw-bold' href='https://mdbootstrap.com/'>
+                    MDBootstrap.com
+                </a>
             </div>
-                </Row>
-                <p class="copyright">Company Name © 2018</p>
-            </Container>
-        </footer>
-    )
-} */
+        </MDBFooter>
+    );
+}
