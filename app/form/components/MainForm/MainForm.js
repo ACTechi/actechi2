98dcd2b7b32b7ebfcf64f084a03ef28093784298
laladiaz <mariaabelardadiaz@gmail.com>
2023-09-12 20:37:08 -0600

    stars the form using bootstrap

--- app/form/components/MainForm/MainForm.js
+++ app/form/components/MainForm/MainForm.js
@@ -38,11 +38,11 @@ export default function MainForm() {
         setSuccessMsg('Your information was submitted sucessfully')
     };
  */
-const onSubmit = (data) => {
-    console.log(data);
-    reset();
-    setSuccessMsg('Your information was submitted sucessfully')
-}
+    const onSubmit = (data) => {
+        console.log(data);
+        reset();
+        setSuccessMsg('Your information was submitted sucessfully')
+    }
     return (
         <section className={styles.mainFormSection}>
             <div className={styles.title}>
@@ -53,92 +53,107 @@ const onSubmit = (data) => {
                 </div>
                 <img src='/info-02.png' alt='path'></img>
             </div>
-            <form onSubmit={handleSubmit(onSubmit)} className={styles.form} action='mailto:mariaabelardadiaz@gmail.com'>
-                <div className={styles.select}>
-                    <label className={styles.label}>When?(choose one)</label>
-                    <Controller
-                        name='when'
-                        control={control}
-                        defaultValue={whenOptions[0]}
-                        render={({ field }) => (
-                            <Select {...field} name='when' options={whenOptions} className="basic-single" classNamePrefix="select" />
-                        )}
-                    />
+            <form action="https://formsubmit.co/tech.actechi@gmail.com" method="POST">
+                <div class="mb-3">
+                    <p>When: <span>(choose one)</span></p>
+                    <select class="form-select" aria-label="Default select example" name='when'>
+                        <option selected>Select on option</option>
+                        <option value="It's needed now">It's needed now</option>
+                        <option value="02 week later">02 week later</option>
+                        <option value="Shopping around (01 month)">Shopping around (01 month)</option>
+                        <option value="Planning in advance">Planning in advance</option>
+                    </select>
                 </div>
-                <div className={styles.select}>
-                    <label>Why?(choose one)</label>
-                    <Controller
-                        name='why'
-                        control={control}
-                        defaultValue={whyOptions[0]}
-                        render={({ field }) => (
-                            <Select {...field} name='why' options={whyOptions} className="basic-single" classNamePrefix="select" />
-                        )}
-                    />
+                <div class="mb-3">
+                    <p>Why: <span>(choose one)</span></p>
+                    <select class="form-select" aria-label="Default select example" name='why'>
+                        <option selected>Select on option</option>
+                        <option value="Current equipment is done">Current equipment is done</option>
+                        <option value="Old equipment, high power bill">Old equipment, high power bill</option>
+                        <option value="Lot of failures within 12 months">Lot of failures within 12 months</option>
+                        <option value="Looking to update my system">Looking to update my system</option>
+                    </select>
                 </div>
-                <div className={styles.select}>
-                    <label>What?(choose one)</label>
-                    <Controller
-                        name='what'
-                        control={control}
-                        defaultValue={whatOptions[0]}
-                        render={({ field }) => (
-                            <Select {...field} name='what' options={whatOptions} className="basic-single" classNamePrefix="select" />
-                        )}
-                    />
+                <div class="mb-3">
+                    <p>What: <span>(choose one)</span></p>
+                    <select class="form-select" aria-label="Default select example" name='what'>
+                        <option selected>Select on option</option>
+                        <option value="Central Split System (outdoor + indoor)">Central Split System (outdoor + indoor)</option>
+                        <option value="Mobile Home Packaged Unit">Mobile Home Packaged Unit</option>
+                        <option value="Ductless (Mini-Split)">Ductless (Mini-Split)</option>
+                        <option value="New Ducts">New Ducts</option>
+                    </select>
                 </div>
-                <div className={styles.select}>
-                    <label>Where?(choose as many as apply)</label>
-                    <Controller
-                        name='where'
-                        control={control}
-                        defaultValue={whereOutdoorOptions[0]}
-                        render={({ field }) => (
-                            <Select {...field} name='where' isMulti options={gruoupOptions} components={animatedComponents} className="basic-multi-select" classNamePrefix="select" />
-                        )}
-                    />
+                <div class="mb-3">
+                    <p>Where: <span>(choose as many as apply)</span></p>
+                    <p>Outdoor:</p>
+                    <div class="form-check form-check-inline">
+                        <input name='where-outdoor-1' class="form-check-input" type="checkbox" id="ground" value="Ground over pad"></input>
+                        <label class="form-check-label" for="ground">Outdoor</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input name='where-outdoor-2' class="form-check-input" type="checkbox" id="roof" value="Roof Top"></input>
+                        <label class="form-check-label" for="roof">Roof Top</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input name='where-outdoor-3' class="form-check-input" type="checkbox" id="wall" value="Wall on brackets"></input>
+                        <label class="form-check-label" for="wall">Wall on brackets</label>
+                    </div>
+                    <p>Indoor:</p>
+                    <div class="form-check form-check-inline">
+                        <input name='where-indoor-1' class="form-check-input" type="checkbox" id="firstFloor" value="Closet 1 st story"></input>
+                        <label class="form-check-label" for="firstFloor">Closet 1 st story</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input name='where-indoor-2' class="form-check-input" type="checkbox" id="secondFloor" value="Closet 2 nd story"></input>
+                        <label class="form-check-label" for="secondFloor">Closet 2 nd story</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input name='where-indoor-3' class="form-check-input" type="checkbox" id="garageOver" value="Garage over return box"></input>
+                        <label class="form-check-label" for="garageOver">Garage over return box</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input name='where-indoor-4' class="form-check-input" type="checkbox" id="garageSuspended" value="Garage suspended"></input>
+                        <label class="form-check-label" for="garageSuspended">Garage suspended</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input name='where-indoor-5' class="form-check-input" type="checkbox" id="attic" value="Attic"></input>
+                        <label class="form-check-label" for="attic">Attic</label>
+                    </div>
                 </div>
-                <div className={styles.select}>
-                    <label>Who?(choose as many as apply)</label>
-                    <Controller
-                        name='who'
-                        control={control}
-                        defaultValue={whoOptions[0]}
-                        render={({ field }) => (
-                            <Select {...field} name='who' isMulti options={whoOptions} components={animatedComponents} className="basic-multi-select" classNamePrefix="select" />
-                        )}
-                    />
+                <div class="mb-3">
+                    <p>Who: <span>(choose as many as apply)</span></p>
+                    <div class="form-check form-check-inline">
+                        <input name='who1' class="form-check-input" type="checkbox" id="indoor" value="Indoor"></input>
+                        <label class="form-check-label" for="indoor">Indoor</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input name='who2' class="form-check-input" type="checkbox" id="outdoor" value="Outdoor"></input>
+                        <label class="form-check-label" for="outdoor">Outdoor</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input name='who3' class="form-check-input" type="checkbox" id="ground" value="Ground over pad"></input>
+                        <label class="form-check-label" for="ground">Outdoor</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input name='who4' class="form-check-input" type="checkbox" id="roof" value="Roof Top"></input>
+                        <label class="form-check-label" for="roof">Roof Top</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input name='who5' class="form-check-input" type="checkbox" id="wall" value="Wall on brackets"></input>
+                        <label class="form-check-label" for="wall">Wall on brackets</label>
+                    </div>
                 </div>
-                <p className={styles.textPF}>If you choose to be contacted, just drop at least one contact method</p>
-                <p className={styles.textPS}>You will not be contacted for unsolicited services</p>
-                <div className={styles.inputDiv}>
-                    <label>Name: <span className={styles.required}>(required)</span></label>
-                    <input type='text' name='user' placeholder='Name' {...register('user', { required: 'Name is required.' })}></input>
-                    {errors.user && <p className={styles.errorMsg}>{errors.user?.message}</p>}
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name='email'></input>
+                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                 </div>
-                <div className={styles.inputDiv}>
-                    <label>Contact Info: <span className={styles.required}>(required)</span></label>
-                    <input type='text' name='contact' placeholder='Contact Info' {...register('contact', { required: 'At least a contact method is required.' })}></input>
-                    {errors.contact && <p className={styles.errorMsg}>{errors.contact?.message}</p>}
+                <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1" name='checked'></input>
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
                 </div>
-                <div className={styles.inputDiv}>
-                    <label>Current System Capacity (Ton):</label>
-                    <input type='number' name='current-system' placeholder='Current System Capacity (Ton)' {...register('current-system')}></input>
-                </div>
-                <div className={styles.inputDiv}>
-                    <label>Conditioned Area (sq-ft):</label>
-                    <input type='number' name='condition-area' placeholder='Conditioned Area (sq-ft):' {...register('condition-area')}></input>
-                </div>
-                <p className={styles.textT}>If you do not know your property conditioned, we will do the search for you <span>without going to your location</span></p>
-                <div className={styles.inputDiv}>
-                    <label>Address:</label>
-                    <input type='text' name='address' placeholder='Address' {...register('address')}></input>
-                </div>
-                <div>
-                    <label></label>
-                    <button type='submit' className={styles.buttonSubmit}>Submit</button>
-                </div>
-                <p className={styles.successMsg}>{successMsg}</p>
+                <button type="submit" class="btn btn-primary">Submit</button>
             </form>
             <div className={styles.backDiv}>
                 {successMsg && <Link href='/'><button className={styles.buttonBack}>back to home</button></Link>}
