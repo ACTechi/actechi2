296d2cf2b2b8e82a9b54917c8a28755fceea2cab
laladiaz <mariaabelardadiaz@gmail.com>
2024-09-15 14:38:32 -0600

    changing the img tags for Image tags

--- app/components/CommercialCarousel/CommercialCarousel.js
+++ app/components/CommercialCarousel/CommercialCarousel.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Carousel from 'react-bootstrap/Carousel';
 
+import Image from 'next/image';
 import styles from './CommercialCarousel.module.css'
 
 export default function CommercialCarousel() {
@@ -9,33 +10,33 @@ export default function CommercialCarousel() {
         <h2 className={styles.title}>Commercial</h2>
         <Carousel pause keyboard touch slide={false}>
             <Carousel.Item>
-            <img src='lightCommercial.jpeg' alt='RTU'></img>
+            <Image src='/lightCommercial.jpeg' alt='RTU' width={400} height={400} />
                 <Carousel.Caption className={styles.bg}>
                 <h3>RTU</h3>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-            <img src='commercial-small-2024.jpg' alt='Small Units for adding extra capacity'></img>
+            <Image src='/commercial-small-2024.jpg' alt='Small Units for adding extra capacity' width={400} height={400} />
                 <Carousel.Caption className={styles.bg}>
                     <h3>Small Units for adding extra capacity</h3>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-            <img src='commercial-appartment-complex-2024.png' alt='Commercial apartment complex'></img>
+            <Image src='/commercial-appartment-complex-2024.png' alt='Commercial apartment complex' width={400} height={400} />
                 <Carousel.Caption className={styles.bg}>
                     <h3>Changeout and new build up for:</h3>
                     <p>Multifamily apartment complex</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-            <img src='buid-up-wearhouse-2024.jpg' alt='commercial buildup'></img>
+            <Image src='/buid-up-wearhouse-2024.jpg' alt='commercial buildup' width={400} height={400} />
                 <Carousel.Caption className={styles.bg}>
                     <h3>Changeout and new build up for:</h3>
                     <p>Warehouses & storage</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-            <img src='commercial-changeout-2024.jpg' alt='commercial changeout'></img>
+            <Image src='/commercial-changeout-2024.jpg' alt='commercial changeout' width={400} height={400} />
                 <Carousel.Caption className={styles.bg}>
                     <h3>Changeout and new build up for:</h3>
                     <p>Warehouses & storage</p>
--- app/components/ResidenctialCarousel/ResidencialCarousel.js
+++ app/components/ResidenctialCarousel/ResidencialCarousel.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Carousel from 'react-bootstrap/Carousel';
 
+import Image from 'next/image';
 import styles from './ResidencialCarousel.module.css'
 
 export default function ResidencialCarousel() {
@@ -9,37 +10,37 @@ export default function ResidencialCarousel() {
         <h2 className={styles.title}>Residential</h2>
         <Carousel pause keyboard touch slide={false}>
             <Carousel.Item>
-            <img src='Central-split-system-2024.JPG' alt='Split System'></img>
+            <Image src='/Central-split-system-2024.JPG' alt='Split System' width={400} height={400} />
                 <Carousel.Caption className={styles.bg}>
                 <h3>Central Split System (Air Conditioner or Heat Pump)</h3>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-            <img src='residential-2nd-2024.png' alt='Split System'></img>
+            <Image src='/residential-2nd-2024.png' alt='Split System' width={400} height={400} />
                 <Carousel.Caption className={styles.bg}>
                 <h3>Central Split System (Air Conditioner or Heat Pump)</h3>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-            <img src='minisplit-residential-2024.JPG' alt='Mini Split System'></img>
+            <Image src='/minisplit-residential-2024.JPG' alt='Mini Split System' width={400} height={400}/>
                 <Carousel.Caption className={styles.bg}>
                     <h3>Mini Split</h3>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-            <img src='mobileHome.jpeg' alt='Packaged unit for mobile home'></img>
+            <Image src='/mobileHome.jpeg' alt='Packaged unit for mobile home' width={400} height={400} />
                 <Carousel.Caption className={styles.bg}>
                     <h3>Packaged unit for mobile home</h3>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-            <img src='rooftpo-unit-2024.JPG' alt='Rooftop units'></img>
+            <Image src='/rooftpo-unit-2024.JPG' alt='Rooftop units' width={400} height={400} />
                 <Carousel.Caption className={styles.bg}>
                     <h3>Rooftop units</h3>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-            <img src='ducts.jpeg' alt='Ducts and all rough ductwork related'></img>
+            <Image src='/ducts.jpeg' alt='Ducts and all rough ductwork related' width={400} height={400} />
                 <Carousel.Caption className={styles.bg}>
                     <h3>Ducts and all rough ductwork related</h3>
                 </Carousel.Caption>
