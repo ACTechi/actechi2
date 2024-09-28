a6eb8e3dd11ab544406bfa706de2e098537ce9f6
laladiaz <mariaabelardadiaz@gmail.com>
2024-09-28 12:32:12 -0600

    fixing call bug

--- app/components/TechVisitButton/TechVisitButton.js
+++ app/components/TechVisitButton/TechVisitButton.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import styles from './TechVisitButton.css'
 import Button from 'react-bootstrap/Button';
-import Link from 'next/link';
+//import Link from 'next/link';
 
 export default function TechVisitButton() {
   return (
-    <Link href="tel:+14072420890">
-            <Button variant="outline-success">Request an A/C Tech Visiting</Button>{' '}
-        </Link>
+    <>
+            <Button variant="outline-success" className={styles.button} href="tel:+14072420890">Request an A/C Tech Visiting</Button>{' '}
+        </>
   )
 }
